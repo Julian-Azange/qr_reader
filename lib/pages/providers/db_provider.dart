9d0b01059c46abc367c1637fbed719af658b2002
Cristian Julian Murcia Palomares <julianazange@gmail.com>
2022-06-10 15:23:07 -0500

    Configuracion de SQFLite
    Se modifico el archivo buil.gradle agregando la linea 52 y 69 para que la aplicacion puediese correr.

---
+++ lib/pages/providers/db_provider.dart
@@ -0,0 +1,17 @@
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider {
+  static Database? _database;
+  static final DBProvider db = DBProvider._();
+  DBProvider._();
+
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+
+    _database = await initDB();
+
+    return _database;
+  }
+
+  Future<Database?> initDB() async {}
+}
